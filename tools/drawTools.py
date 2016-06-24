b13ff8247dde315de9c1268e7acfbd15530454cd
HGangloff <Hgolou@gmail.com>
2016-06-24 15:31:31 +0200

    Improvement of displaying graph : New illustrations

--- tools/drawTools.py
+++ tools/drawTools.py
@@ -12,9 +12,8 @@ def drawFromClasses(G, title = None):
     classes = nx.get_node_attributes(G, 'class')
     black_nodes = [k for k, v in classes.items() if v == 0]
     white_nodes = [k for k, v in classes.items() if v == 1]
-    nx.draw_networkx(G, pos, with_labels = False)
     nx.draw_networkx_nodes(G, pos, black_nodes, node_color = '0', linewidths = 0)
-    nx.draw_networkx_nodes(G, pos, white_nodes, node_color = '1', linewidths = 0)
+    nx.draw_networkx_nodes(G, pos, white_nodes, node_color = '1', linewidths = 0.2)
     nx.draw_networkx_edges(G, pos, edge_color = 'k')
     if title:
         plt.title(title)
@@ -28,7 +27,6 @@ def drawDistanceGraph(G, title = None):
     dist = nx.get_node_attributes(G, 'dist')
     distRange = [v for k, v in dist.items() if v != float("inf")]
     distRange = list(set(distRange)) #kill duplicates
-    nx.draw_networkx(G, pos, with_labels = False)
     #Draw the ones at inf distance :
     thatNodes = [k for k in nx.nodes_iter(G) if G.node[k]['dist'] == float("inf")]
     nx.draw_networkx_nodes(G, pos, thatNodes, node_color = 'r', linewidths = 0)
@@ -54,7 +52,6 @@ def drawLabelledGraph(G, title = None):
     dist = nx.get_node_attributes(G, 'dist')
     lblDict = nx.get_node_attributes(G, 'class')
     lblRange = lblDict[max(lblDict, key = lambda x: lblDict.get(x))]
-    nx.draw_networkx(G, pos, with_labels = False)
     #Draws the nodes with no label
     thatNodes = [k for k in nx.nodes_iter(G) if G.node[k]['class'] == 0]
     nx.draw_networkx_nodes(G, pos, thatNodes, node_color = 'k', linewidths = 0)
@@ -64,7 +61,7 @@ def drawLabelledGraph(G, title = None):
         r = random()
         v = random()
         b = random()
-        color = (r, v, b) #bad coloring if connected component of size 3
+        color = (r, v, b) #Problem with coloring if 3 nodes in the connected component
         nx.draw_networkx_nodes(G, pos, thatNodes, node_color = color, linewidths = 0)
     nx.draw_networkx_edges(G, pos, edge_color = 'k')
     if title:
@@ -79,22 +76,31 @@ def drawZoneofIGraph(G, title = None):
     dist = nx.get_node_attributes(G, 'dist')
     lblDict = nx.get_node_attributes(G, 'class')
     lblRange = lblDict[max(lblDict, key = lambda x: lblDict.get(x))]
-    nx.draw_networkx(G, pos, with_labels = False)
     #Draws the nodes on the border of zones of influence
     thatNodes = [k for k in nx.nodes_iter(G) if G.node[k]['class'] == 0]
     nx.draw_networkx_nodes(G, pos, thatNodes, node_color = 'k', linewidths = 0)
     #Draws the labelled nodes (part of connected comp) and their zones of I
+    alrdyDrawnEdges = []
     for i in range(lblRange):
         thatNodes = [k for k in nx.nodes_iter(G) if G.node[k]['class'] == (i + 1)]
         theirZI = [k for k in nx.nodes_iter(G) if G.node[k]['class'] == -(i + 1)]
+        allThatNodes = thatNodes + theirZI
+        thatEdges = []
+        for v in allThatNodes:
+            for vv in allThatNodes:
+                if G.has_edge(v, vv):
+                    thatEdges.append((v, vv))
+                    alrdyDrawnEdges.append((v, vv))
         r = random()
         v = random()
         b = random()
-        color = (r, v, b)
-        color2 = (min(r + 0.3, 1), min(v + 0.3, 1), min(b + 0.3, 1))
-        nx.draw_networkx_nodes(G, pos, thatNodes, node_color = color, linewidths = 0)
-        nx.draw_networkx_nodes(G, pos, theirZI, node_color = color2, linewidths = 0)
-    nx.draw_networkx_edges(G, pos, edge_color = 'k')
+        color = (r, v, b) #Problem with coloring if 3 nodes in the connected component
+        nx.draw_networkx_nodes(G, pos, thatNodes, node_color = color, linewidths = 0.2)
+        nx.draw_networkx_nodes(G, pos, theirZI, node_color = color, linewidths = 0, alpha = 0.7)
+        nx.draw_networkx_edges(G, pos, thatEdges, edge_color = 'k', alpha = 0.2) #Problem with edge_color = color ?
+    #Draw the remaining edges
+    remainingEdges = [e for e in G.edges() if e not in alrdyDrawnEdges]
+    nx.draw_networkx_edges(G, pos, remainingEdges, edge_color = 'k')
     if title:
         plt.title(title)
     plt.show()
